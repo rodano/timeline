98e37d173ac2dcd2508fb06ed6eb7c61341e0da0
Matthieu Corageoud <matthieu.corageoud@usb.ch>
2026-07-02 16:12:50 +0200

    Illustrate how to add links at runtime

--- example/index.js
+++ example/index.js
@@ -2,12 +2,37 @@ import {Timeline} from '../src/timeline.js';
 
 /**
  * Fetch a JSON configuration file and return its parsed contents.
- * @param {string} config - URL of the JSON configuration file.
+ * @param {string} config_name - URL of the JSON configuration file.
  * @returns {Promise<object>} The parsed configuration.
  */
-async function load(config) {
-	const response = await fetch(config);
-	return await response.json();
+async function load(config_name) {
+	const response = await fetch(config_name);
+	const config = await response.json();
+	enhance_config(config);
+	return config;
+}
+
+/**
+ * Enhance a base configuration object by adding the URL of the link associated with a value
+ * @param {import("../src/types.js").TimelineConfig} config - A timeline configuration object.
+ */
+function enhance_config(config) {
+	(config.sections ?? []).forEach(section => {
+		(section.values ?? []).forEach(value => {
+			if(value.metadata) {
+				if(value.metadata.scopePk) {
+					let link = `#/${value.metadata.scopePk}`;
+					if(value.metadata.visitPk) {
+						link += `/${value.metadata.visitPk}`;
+					}
+					if(value.metadata.formPk) {
+						link += `/${value.metadata.formPk}`;
+					}
+					value.link = link;
+				}
+			}
+		});
+	});
 }
 
 window.addEventListener('load', async() => {
